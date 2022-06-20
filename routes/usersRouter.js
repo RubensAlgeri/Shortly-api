062f48c4ba904a0484125998defac226cc4dfd53
ElderGr <gbielrs@gmail.com>
2022-06-20 13:09:26 -0300

    Preparando revisão de código

--- routes/usersRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { buscarUsuario } from '../controllers/usersController.js';
-import { validaToken } from '../middlewares/authMiddleware.js';
-
-const usersRouter = Router();
-usersRouter.get("/users/:id",validaToken, buscarUsuario);
-// usersRouter.get("/urls/open/:shortUrl", redirecionar)
-export default usersRouter;
